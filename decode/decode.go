38b89a028b355cc10f5ca07bd9596bd278fcb22a
Brendan Higgins <bjh83@case.edu>
2012-11-08 04:43:42 -0500

    NF: I cleaned up the decode function

--- decode/decode.go
+++ decode/decode.go
@@ -2,57 +2,51 @@ package decode
 
 import(
 	"compress/flate"
-	"container/list"
 	"os"
 	"io"
 	"bufio"
 	"bytes"
 	"regexp"
-	"fmt"
+	"strconv"
 )
 
 func Decode(file *os.File) (io.ReadCloser, error) {
 	reader := bufio.NewReader(file)
-	regex, _ := regexp.Compile(".*/Filter/FlateDecode.*")
+	regex, _ := regexp.Compile(".*/Length.*/Filter/FlateDecode.*")
+	var size int64
 	for {
 		line, err := reader.ReadBytes('\n')
 		if err != nil {
 			return nil, err
 		}
 		if regex.Match(line) {
-			fmt.Println("broke")
+			regex, _ = regexp.Compile("[0-9]+")
+			number := regex.Find(line)
+			size, err = strconv.ParseInt(string(number), 10, 32)
+			if err != nil {
+				return nil, err
+			}
 			break
-		}
-	}
-	regex, _ = regexp.Compile("endstream")
-	byteList := list.New()
-	byteList.Init()
-	for {
-		line, err := reader.ReadBytes('\n')
-		if err != nil {
-			return nil, err
-		}
-		if regex.Match(line) {
-			fmt.Println("broke")
-			break
-		}
-		for _, val := range line {
-			byteList.PushBack(val)
 		}
 	}
 	toPrepend := []byte{31, 139, 8, 0, 0, 0, 0, 0, 0, 1, 67, 68, 69, 70, 71}
-	bytebuffer := make([]byte, byteList.Len() - 2 + len(toPrepend))
+	bytebuffer := make([]byte, int(size) - 2 + len(toPrepend))
 	var index int
 	for index, val := range toPrepend {
 		bytebuffer[index] = val
 	}
-	byteList.Remove(byteList.Front())
-	byteList.Remove(byteList.Front())
-	for element := byteList.Front(); element != nil; element = element.Next() {
-		bytebuffer[index] = element.Value.(byte)
-		index++
+	_, err := reader.ReadByte()
+	_, err = reader.ReadByte()
+	if err != nil {
+		return nil, err
 	}
-	fmt.Println(len(bytebuffer))
+	for ;index < len(bytebuffer); index++ {
+		val, err := reader.ReadByte()
+		if err != nil {
+			return nil, err
+		}
+		bytebuffer[index] = val
+	}
 	buffer := bytes.NewBuffer(bytebuffer)
 	return flate.NewReader(buffer), nil
 }
